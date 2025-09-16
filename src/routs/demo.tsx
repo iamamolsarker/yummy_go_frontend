533f8eb1ff07bab32b77db80dc402dc6ded9d259
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-09-16 22:39:40 +0600

    initial work on sifat-ahamed branch

--- src/routs/demo.tsx
+++ src/routs/demo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const demo = () => {
     return (
         <div>
-            <h1 className='text-2xl text-orange-400'>this is demo page</h1>
+            <h1 className=' text-2xl text-orange-400'>this is demo page</h1>
         </div>
     );
 };
