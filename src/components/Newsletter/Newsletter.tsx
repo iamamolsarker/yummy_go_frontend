1ac05aa1e6cc66e2d2c95d2c96b0a5e426dec5ed
yousufali156 <yousufwebwork@gmail.com>
2025-09-16 22:46:06 +0600

    [Add] Newsletter components Add

---
+++ src/components/Newsletter/Newsletter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Newsletter = () => {
+    return (
+        <div>
+            <h1>This is Newsletter Part </h1>
+        </div>
+    );
+};
+
+export default Newsletter;
