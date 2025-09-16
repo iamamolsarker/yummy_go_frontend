1f706fe0a3d2c7e2226617443b871217ea50af9a
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-09-16 22:02:24 +0600

    [Add]:file structure and setup react-ruter,Tailwind CSS

---
+++ src/shared/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className='text-2x text-orange-700'>This is the Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
