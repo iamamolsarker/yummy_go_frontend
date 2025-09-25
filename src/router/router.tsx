6bc077993acdd36c1876ba2c3f2aae9e1fd1f54f
yousufali156 <yousufwebwork@gmail.com>
2025-09-25 19:22:54 +0600

    [Added] FoodMan & Partner Login Form Added Succecfully Done

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,32 +5,43 @@ import Home from "../pages/homePage/Home";
 import Error from "../pages/error page/Error";
 import FoodManForm from "../components/RegisterForm/FoodManForm";
 import PartnerForm from "../components/RegisterForm/PartnerForm";
+import PartnerLoginForm from "../LoginForm/PartnerLoginForm";
+import FoodManLoginForm from "../LoginForm/FoodManLoginForm";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayOut />,
-    children:[
-        {
-            index:true,
-            element:<Home/>
-        },
-        {
-            path: "about",
-            element: <About/>
-        },
-        {
-            path: "/foodman-form",
-            element: <FoodManForm/>
-        },
-        {
-            path: "/partner-form",
-            element: <PartnerForm/>
-        },
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "/foodman-form",
+        element: <FoodManForm />
+      },
+      {
+        path: "/partner-form",
+        element: <PartnerForm />
+      },
+      {
+        path: "/foodman-login",
+        element: <FoodManLoginForm />
+      },
+      {
+        path: "/partner-login",
+        element: <PartnerLoginForm />
+      },
+
     ]
   },
   {
-    path:"*",
-    element:<Error></Error>
+    path: "*",
+    element: <Error></Error>
   }
 ]);
