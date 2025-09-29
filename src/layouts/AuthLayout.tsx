fca7a7b6d226e7857a5f57733ef9dd4ad847b80a
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-09-29 23:03:11 +0600

    [Update]:pollish login,reg and error page

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router';
+import Navbar from '../components/shared/navbar/Navbar';
 
 const AuthLayout: React.FC = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
         </div>
     );
